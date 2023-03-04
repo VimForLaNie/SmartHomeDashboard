c79ad8dfa1679346617be1e41d8c1876303c00b4
VimForLaNie <new.giant098@gmail.com>
2023-03-04 17:34:15 +0700

    add light

---
+++ src/components/Light.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let name = "Light";
+    let state = false;
+    let light: HTMLDivElement;
+    let text: HTMLParagraphElement;
+
+    const onClick = () => {
+        state = !state;
+        light.style.height = state ? "100%" : "10%";
+        light.style.width = state ? "100%" : "10%";
+        // text.classList.add("text-black");
+    }
+</script>
+
+<button class="bg-zinc-800 w-1/3 self-center h-24 flex overflow-hidden" on:click={onClick}>
+    <p class="flex p-8 text-lg absolute" bind:this={text}>{name}</p>
+    <div class="bg-yellow-100 opacity-30 rounded-full blur-lg self-center m-auto animate" bind:this={light}>
+    </div>
+</button>
+
+<style lang="postcss">
+    .animate{
+        transition: width 0.5s ease-in-out;
+        transition: height 0.5s ease-in-out;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
 	// import ACcontroller from '../components/ACcontroller.svelte';
-	import { onMount } from 'svelte';
-	import Time from '../components/time.svelte';
-	import OnOffcontroller from '../components/OnOffcontroller.svelte';
-	import WattMeter from '../components/WattMeter.svelte';
+	// import { onMount } from 'svelte';
+	// import Time from '../components/time.svelte';
+	// import OnOffcontroller from '../components/OnOffcontroller.svelte';
+	// import WattMeter from '../components/WattMeter.svelte';
+	// import Light from '../components/Light.svelte';
 
 	let data: any = {};
 	let temp = "-";
 	let aq = "-";
-	onMount(async () => {
-		setInterval(async () => {
-			const res = await fetch('http://34.124.227.14:3000/');
-			data = await res.json();
-			temp = data.temp || "-";
-			aq = data.aq || "-";
-			console.log(data);
-		}, 1000);
-	});
+	// onMount(async () => {
+	// 	setInterval(async () => {
+	// 		const res = await fetch('http://34.124.227.14:3000/');
+	// 		data = await res.json();
+	// 		temp = data.temp || "-";
+	// 		aq = data.aq || "-";
+	// 		console.log(data);
+	// 	}, 1000);
+	// });
 	
 </script>
 
@@ -25,6 +26,7 @@
 	<OnOffcontroller SwitchName="AC"/>
 	<OnOffcontroller room="Office" device="Fan" SwitchName="Fan"/>
 	<WattMeter value="12"/>
+	<Light name="Hi"/>
 </div>
 
 
