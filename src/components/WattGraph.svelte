290ed365d48746778ba0947b1e4f9caf4cd2cbf2
VimForLaNie <new.giant098@gmail.com>
2023-03-10 21:23:54 +0700

    graph

--- src/components/WattGraph.svelte
+++ src/components/WattGraph.svelte
@@ -18,18 +18,20 @@
         for(const [i, topic] of topicArray.entries()){
             let temp = await getData(`/api/data?topic=${topic}&amount=1000&sort=-1`);
 			// console.log(temp)
+			// old -> new
 			for(let idx = temp.length - 1; idx >= 0; idx--){
 				temp[idx]["group"] = nameArray[i];
 				let currWatt = parseInt(temp[idx]["payload"]) / 100;
+				// console.log(currWatt);
 				let currTime = (new Date(temp[idx]["time"])).getTime();
 				if(idx == 0) {
-					temp[idx]["payload"] = (currWatt * ((Date.now() - currTime) / 3600000) * 4) + (temp[idx+1]?.payload ?? 0);
-					meterValue[i] = currWatt;
+					temp[idx]["payload"] = ((currWatt / 1000) * ((Date.now() - currTime) / 36000000) * 4) + (temp[idx+1]?.payload ?? 0);
+					meterValue[i] = currWatt / 1000 * 60;
 					break;
 				}
-				let prevTime = (new Date(temp[idx-1]["time"])).getTime();
-				let dT = prevTime - currTime;
-				temp[idx].payload = (currWatt * (dT / 3600000) * 4) + (temp[idx+1]?.payload ?? 0);
+				let nextTime = (new Date(temp[idx-1]["time"])).getTime();
+				let dT = nextTime - currTime;
+				temp[idx].payload = ((currWatt / 1000) * (dT / 3600000) * 4) + (temp[idx+1]?.payload ?? 0);
 			}
 			// console.log(temp[temp.length-1]["payload"])
             res = [...res,...temp];
